20f9e48084f15ef85bbfd3299b62c9423673903d
Davis <Davis@qq.com>
2025-12-20 09:30:05 +0800

    初始化项目

---
+++ src/main/java/com/example/devgroveback/DevGroveBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.devgroveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevGroveBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevGroveBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devgroveback/DevGroveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devgroveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevGroveBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
